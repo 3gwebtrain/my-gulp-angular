f199c35ac78705a781d5898e33682e12b6d4a8e5
mohamear <mohamear@cisco.com>
2016-06-08 17:25:53 +0530

    gulp angular base files are added

---
+++ gulp/paths.js
@@ -0,0 +1,21 @@
+var paths = {
+
+	"prod" : {
+		"path" 		: "./prod",
+		"config"	: "./source/config/config.production.json"
+	},
+
+	"test" : {
+		"path" : "./test",
+		"config"	: "./source/config/config.testing.json"
+	},
+
+	"mock" : {
+		"path" 		: "./mock",
+		"config"	: "./source/config/config.mock.json"
+	},
+
+
+}
+
+module.exports = paths;
---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var paths 		= require('./gulp/paths'),
+	args 		= require('yargs').argv,
+	fileSytems 	= require('file-system');
+
+var ENV = args.env || "mock";
+
+var config = JSON.parse(fileSytems.readFileSync( paths[ENV].config, 'utf8'));
+
+console.log( paths[ENV].config  , ENV, config );
